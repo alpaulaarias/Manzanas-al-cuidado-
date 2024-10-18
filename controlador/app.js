e5f1d5edd80923de69553806d0e1985b7ed9b705
Majokairuz <mherrerakairuz@gmail.com>
2024-10-17 21:18:15 -0500

    controlador

--- controlador/app.js
+++ controlador/app.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mysql2 = require('mysql2/promise')
-
+const path= require('path')
+const moment=require('moment')
+const session=require('express-session')
 const app = express()
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname)));
+
+// configurar la sesion
+app.use(session({
+    secret:'Miapp',
+    resave:false,
+    saveUninitialized:true
+}))
 
 //Conexion bbdd
 const db = mysql2.createPool({
@@ -17,18 +27,14 @@ const db = mysql2.createPool({
     database: 'manzanasdelcuidado'
 })
 
-db.on("error", (err) => {
-    console.error("Error 7: " + err)
-})
 
 // Registrar usuario
 app.post('/crear', async (req, res) => {
     const {nombres,tipod,numerod,manzanaa} = req.body
     try {
-        console.log(manzanaa);
-        
         //verificar el usurio
-        const [veri]= await db.query('SELECT * FROM usuario WHERE Tipo_documento=? AND Documento=?', [tipod,numerod])
+        const conect= await mysql2.createConnection(db)
+        const [veri]= await conect.execute('SELECT * FROM usuario WHERE Tipo_documento=? AND Documento=?', [tipod,numerod])
 
         if (veri.length > 0) {
             res.status(409).send(`
@@ -41,7 +47,7 @@ app.post('/crear', async (req, res) => {
                 `)
         }
         else{
-            await db.query('INSERT INTO usuario (Nombres,Tipo_documento,Documento,fk_id_manzana) VALUES (?,?,?,?)',[nombres, tipod, numerod,manzanaa])
+            await conect.execute('INSERT INTO usuario (Nombres,Tipo_documento,Documento,fk_id_manzana) VALUES (?,?,?,?)',[nombres, tipod, numerod,manzanaa])
             res.status(201).send(`
                 <script>
                 window.onload=function(){
@@ -50,26 +56,28 @@ app.post('/crear', async (req, res) => {
                 }
                 </script>
                 `)
+                await conect.end()
         }
+        await conect.end()
     }
     catch (error) {
         console.error('Error en el servidor:', error)
         res.status(500).send('Error en el servidor');
     }
-//     const jhon = `INSERT INTO usuario (Nombres,Tipo_documento,Documento,fk_id_manzana) VALUES (?,?,?,?)`
-//     db.query(jhon, [nombre, tipo, numero, manzana], (err, result) => {
-//         if (err) {
-//             console.error("Error: " + err)
-//             res.status(500).send("Pailas")
-//             return
-//         }
-//         else {
-//             console.log("Todo bien perro")
-//             res.status(200).send("Listo los datos")
-//         }
-//     })
 })
 
+app.post('/iniciar', async (req,res)=>{
+    const {tipod,numerod}=req.body
+    try{
+        const conect=await mysql2.createConnection(db)
+        const [datos]= await conect.execute('SELECT * FROM usuario WHERE Tipo_documento=? AND Documento=?', [tipod,numerod])
+        console.log(datos)
+    }
+    if (datos.length>0){
+        const [man]=
+    res.sendFile(path.join(__dirname, 'botones.html'))
+}
+})
 // Apertura del servidor
 app.listen(3000, () => {
     console.log(`Servidor Node.js escuchando`)
